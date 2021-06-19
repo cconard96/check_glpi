55bb941ab9e91dac2195dea18e8be0f3d459aefa
Curtis Conard <cconard96@gmail.com>
2021-06-18 22:24:57 -0400

    Break out url parts

--- check_glpi.pl
+++ check_glpi.pl
@@ -12,25 +12,45 @@ my $version = "1.0.0-beta-1";
 my $user_agent = "check_glpi/1.0";
 
 my $np = Nagios::Monitoring::Plugin->new(
-    usage => "Usage: %s -u|--url <http://host:port/url> "
+    usage => "Usage: %s -h|--host <host> "
+    . "[ -p|--port <port> ] "
+    . "[ -d|--subdirectory <subdirectory> ] "
+    . "[ -P|--protocol <HTTP|HTTPS> ] "
     . "[ -s|--service <service> ] "
     . "[ --ignoressl ] "
     . "[ -h|--help ] ",
     version => $version,
     blurb   => 'Nagios plugin to check GLPI service status via http(s)',
     extra   => "\nExample: \n"
-    . "check_json.pl --url http://192.168.1.100:80/glpi -s db' ",
+    . "check_json.pl --host 192.168.1.100 --protocol https --port 8443 --subdirectory glpi -s db' ",
     url     => 'https://github.com/cconard96/check_glpi',
     plugin  => 'check_glpi',
     timeout => 15,
-    shortname => "Check GLPI status API",
+    shortname => "Check GLPI status",
 );
 
- # add valid command line options and build them into your usage/help documentation.
 $np->add_arg(
-    spec => 'url|u=s',
-    help => '-u, --url http://user:pass@192.168.1.100:80/glpi',
+    spec => 'host|h=s',
+    help => '-h, --host localhost',
     required => 1,
+);
+
+$np->add_arg(
+    spec => 'port|p=i',
+    help => '-p, --port 80',
+    default => 80,
+);
+
+$np->add_arg(
+    spec => 'protocol|P=s',
+    help => '-P, --protocol 80',
+    default => 'HTTP'
+);
+
+$np->add_arg(
+    spec => 'subdirectory|d=s',
+    help => '-d, --subdirectory glpi',
+    required => 0,
 );
 
 $np->add_arg(
@@ -44,11 +64,9 @@ $np->add_arg(
     help => "--ignoressl\n   Ignore bad ssl certificates",
 );
 
-## Parse @ARGV and process standard arguments (e.g. usage, help, version)
 $np->getopts;
 if ($np->opts->verbose) { (print Dumper ($np))};
 
-## GET URL
 my $ua = LWP::UserAgent->new;
 
 $ua->env_proxy;
@@ -66,7 +84,11 @@ if ($np->opts->verbose) { (print Dumper ($ua))};
 
 my $attribute = '{status}';
 
-my $request_url = $np->opts->url."?format=json";
+my $request_url = lc($np->opts->protocol).'://'.$np->opts->host.':'.$np->opts->port;
+if ($np->opts->subdirectory) {
+    $request_url = $request_url . '/' . $np->opts->subdirectory;
+}
+$request_url = $request_url . '/status.php?format=json';
 if ($np->opts->service) {
     $request_url = $request_url."&service=".$np->opts->service;
 } else {
