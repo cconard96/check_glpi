c423c0419bcffc9da43adb74627adbcf44332a3b
Curtis Conard <cconard96@gmail.com>
2021-06-18 21:19:40 -0400

    Initial commit

---
+++ check_glpi.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use HTTP::Request::Common;
+use LWP::UserAgent;
+use JSON;
+use Nagios::Monitoring::Plugin;
+use Data::Dumper;
+
+my $version = "1.0.0-beta-1";
+my $user_agent = "check_glpi/1.0";
+
+my $np = Nagios::Monitoring::Plugin->new(
+    usage => "Usage: %s -u|--url <http://host:port/url> "
+    . "[ -s|--service <service> ] "
+    . "[ --ignoressl ] "
+    . "[ -h|--help ] ",
+    version => $version,
+    blurb   => 'Nagios plugin to check GLPI service status via http(s)',
+    extra   => "\nExample: \n"
+    . "check_json.pl --url http://192.168.1.100:80/glpi -s db' ",
+    url     => 'https://github.com/cconard96/check_glpi',
+    plugin  => 'check_glpi',
+    timeout => 15,
+    shortname => "Check GLPI status API",
+);
+
+ # add valid command line options and build them into your usage/help documentation.
+$np->add_arg(
+    spec => 'url|u=s',
+    help => '-u, --url http://user:pass@192.168.1.100:80/glpi',
+    required => 1,
+);
+
+$np->add_arg(
+    spec => 'service|s=s',
+    help => '-s, --service db',
+    required => 0,
+);
+
+$np->add_arg(
+    spec => 'ignoressl',
+    help => "--ignoressl\n   Ignore bad ssl certificates",
+);
+
+## Parse @ARGV and process standard arguments (e.g. usage, help, version)
+$np->getopts;
+if ($np->opts->verbose) { (print Dumper ($np))};
+
+## GET URL
+my $ua = LWP::UserAgent->new;
+
+$ua->env_proxy;
+$ua->agent($user_agent);
+$ua->default_header('Accept' => 'application/json');
+$ua->protocols_allowed( [ 'http', 'https'] );
+$ua->parse_head(0);
+$ua->timeout($np->opts->timeout);
+
+if ($np->opts->ignoressl) {
+    $ua->ssl_opts(verify_hostname => 0, SSL_verify_mode => 0x00);
+}
+
+if ($np->opts->verbose) { (print Dumper ($ua))};
+
+my $attribute = '{status}';
+
+my $request_url = $np->opts->url."?format=json";
+if ($np->opts->service) {
+    $request_url = $request_url."&service=".$np->opts->service;
+} else {
+    $attribute = '{glpi}->{status}';
+}
+my $response = $ua->request(GET $request_url);
+
+## Parse JSON
+my $json_response = decode_json($response->content);
+if ($np->opts->verbose) { (print Dumper ($json_response))};
+
+my $check_value;
+my $result = -1;
+my $resultTmp;
+
+my $check_value_str = '$check_value = $json_response->'.$attribute;
+
+if ($np->opts->verbose) { (print Dumper ($check_value_str))};
+eval $check_value_str;
+
+if (!defined $check_value) {
+    $np->nagios_exit(UNKNOWN, "No value received");
+}
+
+if ($check_value eq "OK") {
+    $result = 0;
+} elsif ($check_value eq "WARNING") {
+    $result = 1;
+} elsif ($check_value eq "ERROR") {
+    $result = 2;
+} else {
+    $result = -1;
+}
+
+$np->nagios_exit(
+    return_code => $result,
+    message     => $response->content,
+);
